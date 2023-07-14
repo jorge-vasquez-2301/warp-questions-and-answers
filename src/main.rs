f78e65899622e471a286d26558dabf2c24b773de
Jorge Vasquez <jv2301@gmail.com>
2023-07-14 15:06:34 -0400

    Split lib from main

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,14 @@
-#![warn(clippy::all)]
-
-use handle_errors::return_error;
-use tracing_subscriber::fmt::format::FmtSpan;
-use warp::{hyper::Method, Filter};
-
-mod config;
-mod profanity;
-mod routes;
-mod store;
-mod types;
+use rust_web_dev::{config, run, setup_store};
 
 #[tokio::main]
 async fn main() -> Result<(), handle_errors::Error> {
     dotenv::dotenv().ok();
 
     let config = config::Config::new().expect("Config can't be set");
-
-    let log_filter = format!(
-        "handle_errors={},rust_web_dev={},warp={}",
-        config.log_level, config.log_level, config.log_level
-    );
-
-    let store = store::Store::new(&format!(
-        "postgres://{}:{}@{}:{}/{}",
-        config.db_user, config.db_password, config.db_host, config.db_port, config.db_name
-    ))
-    .await
-    .map_err(handle_errors::Error::DatabaseQueryError)?;
-
-    sqlx::migrate!()
-        .run(&store.clone().connection)
-        .await
-        .map_err(handle_errors::Error::MigrationError)?;
-
-    let store_filter = warp::any().map(move || store.clone());
-
-    tracing_subscriber::fmt()
-        // Use the filter we built above to determine which traces to record.
-        .with_env_filter(log_filter)
-        // Record an event when each span closes. This can be used to time our
-        // routes' durations!
-        .with_span_events(FmtSpan::CLOSE)
-        .init();
-
-    let cors = warp::cors()
-        .allow_any_origin()
-        .allow_header("content-type")
-        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
-
-    let get_questions = warp::get()
-        .and(warp::path("questions"))
-        .and(warp::path::end())
-        .and(warp::query())
-        .and(store_filter.clone())
-        .and_then(routes::get_questions);
-
-    let update_question = warp::put()
-        .and(warp::path("questions"))
-        .and(warp::path::param::<i32>())
-        .and(warp::path::end())
-        .and(routes::auth())
-        .and(store_filter.clone())
-        .and(warp::body::json())
-        .and_then(routes::update_question);
-
-    let delete_question = warp::delete()
-        .and(warp::path("questions"))
-        .and(warp::path::param::<i32>())
-        .and(warp::path::end())
-        .and(routes::auth())
-        .and(store_filter.clone())
-        .and_then(routes::delete_question);
-
-    let add_question = warp::post()
-        .and(warp::path("questions"))
-        .and(warp::path::end())
-        .and(routes::auth())
-        .and(store_filter.clone())
-        .and(warp::body::json())
-        .and_then(routes::add_question);
-
-    let add_answer = warp::post()
-        .and(warp::path("answers"))
-        .and(warp::path::end())
-        .and(routes::auth())
-        .and(store_filter.clone())
-        .and(warp::body::form())
-        .and_then(routes::add_answer);
-
-    let registration = warp::post()
-        .and(warp::path("registration"))
-        .and(warp::path::end())
-        .and(store_filter.clone())
-        .and(warp::body::json())
-        .and_then(routes::register);
-
-    let login = warp::post()
-        .and(warp::path("login"))
-        .and(warp::path::end())
-        .and(store_filter.clone())
-        .and(warp::body::json())
-        .and_then(routes::login);
-
-    let routes = get_questions
-        .or(update_question)
-        .or(add_question)
-        .or(delete_question)
-        .or(add_answer)
-        .or(registration)
-        .or(login)
-        .with(cors)
-        .with(warp::trace::request())
-        .recover(return_error);
+    let store = setup_store(&config).await?;
 
     tracing::info!("Q&A service build ID {}", env!("RUST_WEB_DEV_VERSION"));
-
-    warp::serve(routes).run(([0, 0, 0, 0], config.port)).await;
+    run(config, store).await;
 
     Ok(())
 }
